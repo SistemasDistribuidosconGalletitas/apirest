028e84f3cafe8b5b591669ae0ab2ee8de5ecbda7
Cristhian <cristhianE2018>
2021-07-23 00:55:51 -0500

    Se agrego la dependencia JSONObject y una consulta para regresar un Json personalizado de prueba

--- src/main/java/com/example/demo/Controladores/MedicosControlador.java
+++ src/main/java/com/example/demo/Controladores/MedicosControlador.java
@@ -3,12 +3,19 @@ package com.example.demo.Controladores;
 
 import com.example.demo.Entidades.Medico;
 import com.example.demo.Repositorios.MedicosRepositorio;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.catalina.mapper.Mapper;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path= "/api/ssm")
@@ -21,4 +28,15 @@ public class MedicosControlador {
         System.out.println("Hola!");
         return medicosRepositorio.findAll();
     }
+
+    @GetMapping("/medicos/ejemplo")
+    public Map<String, String> sayHello() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("paciente", "Cristhian Enrique");
+        map.put("Medico", "Doctor Simi");
+        map.put("Fecha consulta","12/11/2021");
+        map.put("medicamento", "Parecetamol");
+        return map;
+    }
+
 }
